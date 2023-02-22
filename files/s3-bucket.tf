e878390f94b772cb9ef320de5f694c3656867761
Sean <sean_w@nimble.com.au>
2023-02-22 03:55:47 +0000

    Added s3 bucket module

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -2,3 +2,8 @@ module "s3-bucket" {
   source  = "app.terraform.io/seanybwalker/s3-bucket/aws"
   version = "2.8.0"
 }
+
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "ykSB0Z3qUXH7wQ.atlasv1.l5AiWT6Ga5uC8SjzVQ1noexMZ4azmyuDe66u1fjFpabIz6EkHsBvtOHJPpw3OdLrhiM"
+  }
